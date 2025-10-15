21ffe5dc4263a4d51e969c3984f3be27ede41287
Ronin Campbell <ronincampbell3@gmail.com>
2025-10-14 21:24:22 -0700

    New UI, Calculator Logic, and Unlocking Operators & Digits.

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,37 +1,159 @@
-﻿using System.Windows;
-using System.IO;
-using Calculator_.src;
-using System.Data.SQLite;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Calculator_
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentNumber = "";
+        private string operation = "";
+        private double result = 0;
+        private bool isOperatorClicked = false;
+        public int UnlockedDigits = 1;
+
         public MainWindow()
         {
             InitializeComponent();
-            if (SystemParameters.WorkArea.Height < SystemParameters.WorkArea.Width)
+            UpdateResultDisplay(currentNumber);
+        }
+
+        private void UpdateResultDisplay(string text)
+        {
+            TextBlock[] resultDigits = { ResultDigit0, ResultDigit1, ResultDigit2, ResultDigit3, ResultDigit4, ResultDigit5, ResultDigit6, ResultDigit7 };
+            Border[] resultDigitBorders = { ResultDigit0Border, ResultDigit1Border, ResultDigit2Border, ResultDigit3Border, ResultDigit4Border, ResultDigit5Border, ResultDigit6Border, ResultDigit7Border };
+
+            for (int i = 0; i < resultDigits.Length; i++)
             {
-                this.Height = SystemParameters.WorkArea.Height / 4 * 3;
-                // I am aware that WorkArea.Width exists, this is in case of a non-16:9 display to keep the window ratio consistent
-                this.Width = this.Height / 9 * 16;
-            }else{
-				this.Width = SystemParameters.WorkArea.Width / 4 * 3;
-                this.Height = this.Width / 16 * 9;
-			}
-            SQLiteCommand tableProbe = Database.RawHook.CreateCommand();
-            tableProbe.CommandText = "SELECT count(id) FROM Users;";
-            bool result = Convert.ToBoolean(tableProbe.ExecuteScalar());
-            if (!result){
-				UserProfile initData = new(true)
-				{
-					Title = "Create first user"
-				};
-				initData.ShowDialog();
+                resultDigits[i].Text = "";
+                if (i < UnlockedDigits)
+                {
+                    resultDigitBorders[i].Background = new SolidColorBrush(Colors.White);
+                }
+                else
+                {
+                    resultDigitBorders[i].Background = new SolidColorBrush(Colors.LightGray);
+                }
             }
-		}
+
+            int len = text.Length;
+            for (int i = 0; i < len; i++)
+            {
+                char c = text[len - 1 - i];
+                if (i < resultDigits.Length)
+                {
+                    resultDigits[i].Text = c.ToString();
+                }
+            }
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            string buttonContent = (sender as Button).Content.ToString();
+            if (currentNumber.Length >= UnlockedDigits && !isOperatorClicked)
+            {
+                MessageBox.Show($"Only {UnlockedDigits} Digit(s) unlocked! Please purchase additonal digits and try again!", "Error.", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (isOperatorClicked)
+            {
+                currentNumber = "";
+                isOperatorClicked = false;
+            }
+            currentNumber += buttonContent;
+            UpdateResultDisplay(currentNumber);
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber != "")
+            {
+                result = double.Parse(currentNumber);
+                isOperatorClicked = true;
+                operation = (sender as Button).Content.ToString();
+                UpdateResultDisplay(operation);
+            }
+        }
+
+        private void Purchase_Click(object sender, RoutedEventArgs e)
+        {
+            // API intergration later.
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber != "" && !isOperatorClicked && operation != "")
+            {
+                double secondNumber = double.Parse(currentNumber);
+                if (operation == "+" && (result + secondNumber) == (result * secondNumber))
+                {
+                    ButtonMultiply.IsEnabled = true;
+                }
+
+                if (operation == "-" && (result - secondNumber) == (result / secondNumber))
+                {
+                    ButtonDivide.IsEnabled = true;
+                }
+
+                switch (operation)
+                {
+                    case "+":
+                        result += secondNumber;
+                        break;
+                    case "-":
+                        result -= secondNumber;
+                        break;
+                    case "x":
+                        result *= secondNumber;
+                        break;
+                    case "/":
+                        if(secondNumber != 0)
+                        {
+                            result /= secondNumber;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Cannot divide by zero");
+                            Clear_Click(null, null);
+                            return;
+                        }
+                        break;
+                }
+                currentNumber = result.ToString();
+                UpdateResultDisplay(currentNumber);
+                operation = "";
+
+                if (result.ToString().Split('.')[0].Length > UnlockedDigits)
+                {
+                    MessageBox.Show($"Only {UnlockedDigits} Digit(s) unlocked! Please purchase additonal digits and try again!", "Error.", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    Clear_Click(null, null);
+                    return;
+                }
+            }
+            isOperatorClicked = false;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            currentNumber = "";
+            result = 0;
+            operation = "";
+            UpdateResultDisplay(currentNumber);
+            isOperatorClicked = false;
+        }
+
+        private void Decimal_Click(object sender, RoutedEventArgs e)
+        {
+            if (!currentNumber.Contains("."))
+            {
+                if (currentNumber == "")
+                {
+                    currentNumber = "0";
+                }
+                currentNumber += ".";
+                UpdateResultDisplay(currentNumber);
+            }
+        }
     }
 }
